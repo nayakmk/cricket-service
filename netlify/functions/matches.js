4f49a79ce1d30e3a9151aa10b78cde23bb30ee3a
Malaya Nayak <malaya.nayak@outlook.com>
2025-10-04 08:35:12 +0530

    feat: Update CORS configuration to allow all origins and disable credentials

--- netlify/functions/matches.js
+++ netlify/functions/matches.js
@@ -1,10 +1,10 @@
 const { collections } = require('../../config/database');
 
 const corsHeaders = {
-  'Access-Control-Allow-Origin': 'http://localhost:3001',
+  'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Headers': 'Content-Type, Authorization, X-Requested-With',
   'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-  'Access-Control-Allow-Credentials': 'true',
+  'Access-Control-Allow-Credentials': 'false',
   'Content-Type': 'application/json',
 };
 
